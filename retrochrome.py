51fb4d532ddc58c60db083f98173d98dcf07ad7a
Ewan Drummond <45878916+edrumm@users.noreply.github.com>
2023-02-19 22:47:59 +0000

    use gimpfu constants

--- retrochrome.py
+++ retrochrome.py
@@ -15,25 +15,47 @@ def get_active_drawable(image):
 
 def retrochrome(image, drawable, burn):
     # TODO prefix with image_ ?
-    height = pdb.gimp_image_height(image)
-    width = pdb.gimp_image_width(image)
+    height = image.height
+    width = image.width
 
-    rc_layer_temp = pdb.gimp_layer_new(image, width, height, 0, "retrochrome-temp", 100, 0)
+    rc_layer_temp = pdb.gimp_layer_new(image, width, height, RGB_IMAGE, "retrochrome-temp", 100, LAYER_MODE_NORMAL_LEGACY)
 
     # TODO determine layer position, temp value for now
+    # use 1+pdb.gimp_image_get_item_position(img, layer) to insert above
     rc_layer_temp_pos = 1
     
     # TODO check None is correct for parent, should be
     pdb.gimp_image_insert_layer(image, rc_layer_temp, None, rc_layer_temp_pos)
-    rc_temp_layer = pdb.gimp_image_merge_down(image, drawable, 0)
+    rc_temp_layer = pdb.gimp_image_merge_down(image, drawable, EXPAND_AS_NECESSARY)
 
     drawable = get_active_drawable(image)
-    pdb.plug_in_gauss(image, drawable, 5, 5, 0)
 
-    pdb.gimp_image_select_color(image, 0, drawable, BLACK)
+    pdb.plug_in_gauss_rle(image, drawable, 5, 1, 1)
+
+    pdb.gimp_image_select_color(image, CHANNEL_OP_ADD, drawable, BLACK)
     pdb.gimp_selection_invert(image)
+    pdb.gimp_layer_set_visible(rc_temp_layer, 0)
 
-    return
+    # Work in progress
+    # rc_chrome_layer = pdb.gimp_layer_new(image, width, height, RGB_IMAGE, "retrochrome-chrome", 100, LAYER_MODE_NORMAL_LEGACY)
+    # pdb.gimp_image_insert_layer(image, rc_chrome_layer, None, rc_layer_temp_pos + 1)
+
+    # drawable = get_active_drawable(image)
+
+    rc_gradient = pdb.gimp_gradient_new("rc_gradient")
+    # TODO add colour list
+    rc_gradient_colours = [
+        WHITE,
+        BLACK
+    ]
+
+    # Work in progress
+    # TODO set segments according to rc_gradient colours, 0 for now
+    # pdb.gimp_gradient_segment_set_left_color(rc_gradient, 0, WHITE, 100)
+    # pdb.gimp_gradient_segment_set_right_color(rc_gradient, 0, BLACK, 100)
+    # pdb.gimp_context_set_gradient(rc_gradient)
+    # TODO set supersampling and gradient coordinates
+    # pdb.gimp_drawable_edit_gradient_fill(drawable, GRADIENT_LINEAR, 0, 0, 1, 1, 1, 0, 0, width // 2, height)
 
 
 # error handling
